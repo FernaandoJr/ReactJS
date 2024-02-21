86f71c2a045b9ef995872b2c93c1a59e8f38b308
Fernando <kinggprobr@gmail.com>
2024-02-21 12:45:07 -0300

    aula 40

--- meuapp/src/App.jsx
+++ meuapp/src/App.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
-import Membro from './components/Membro'
 
 class App extends Component{
-    render(){
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      status: false,
+      nome: "Fernando"
+    }
+    this.entrar = this.entrar.bind(this)
+    this.sair = this.sair.bind(this)
+    
+
+  }
+
+  entrar(){
+    this.setState({status: true})
+  }
+  sair(){
+    this.setState({status: false})
+  }
+
+  render(){
       return(
         <div>
-          <Membro nome="Fernando"/>
+          {this.state.status ?
+          <div>
+            <h1>Bem vindo {this.state.nome}</h1>
+            <button onClick={this.sair}>Sair</button>
+          </div> :
+          <div>
+            <h1>Bem vindo deconhecido</h1>
+            <button onClick={this.entrar}>Sair</button>
+          </div>
+          }
         </div>
       )
     }
