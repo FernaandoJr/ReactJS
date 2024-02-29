b4be6372079ae75edc7f58c45f5f0d9611bf95dd
Fernando <kinggprobr@gmail.com>
2024-02-29 14:18:25 -0300

    aula 48

--- meuapp/src/App.jsx
+++ meuapp/src/App.jsx
@@ -1,63 +1,27 @@
-import React, { Component } from 'react'
-import './estilo.css'
-class App extends Component{
+import React, { useState } from 'react';
 
-  constructor(props){
-    super(props)
-    this.state = {
-      numero: 0,
-      botao: 'Vai'
-    }
-    this.timer = null
-    this.vai = this.vai.bind(this)
-    this.limpar = this.limpar.bind(this)
-    }
+function App(){
 
-  vai(){
-    let state = this.state;
-
-    if(this.timer !== null){
-      clearInterval(this.timer);
-      this.timer = null
-      state.botao = "Vai"
-    } else{
-      this.timer = setInterval(() => {
-        let state = this.state;
-        state.numero+= 0.1
-        this.setState(state)
-      }, 100);
-      state.botao = "Pausar"
-    }
-
-    this.setState(state)
-
-    
-  }
+  const [tarefas, setTarefas] = useState(['Pagar a conta de luz', 'testando'])
+  const [input, setInput] = useState('')
   
-  limpar(){
-    if(this.timer !== null){
-      clearInterval(this.timer);
-      this.timer = null
-    }
 
-    let state = this.state
-    state.numero = 0
-    state.botao = "Vai"
-    this.setState(state)
+  function addTarefa(){
+      setTarefas([...tarefas, input])
+      setInput('')
   }
-  
-  render(){
-    return(
-      <div className='container'>
-        <img src={require('./assets/cronometro.png')} className='img'/>
-        <a className='timer'>{this.state.numero.toFixed(1)}</a>
-        <div className="areaBtn">
-        <a className='botao' onClick={this.vai}>{this.state.botao}</a>
-        <a className='botao' onClick={this.limpar}>Limpar</a>
-        </div>
-      </div>
-    )
-  }
+
+  return(
+    <div>
+      <ul>
+        {tarefas.map(tarefa =>(
+          <li key={tarefa}>{tarefa}</li>
+          ))}
+      </ul>
+      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+      <button onClick={addTarefa}>Adicionar Tarefa</button>
+    </div>
+  )
 }
 
 export default App
