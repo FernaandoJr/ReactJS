c331dec49a0dbaf001f086e39e08a552a96d2f23
Fernando <kinggprobr@gmail.com>
2024-02-29 13:26:33 -0300

    aula 46

--- meuapp/src/App.jsx
+++ meuapp/src/App.jsx
@@ -1,44 +1,60 @@
 import React, { Component } from 'react'
 import './estilo.css'
-
 class App extends Component{
 
   constructor(props){
     super(props)
     this.state = {
-      textoFrase: ''
-    };
-    this.quebraBiscoito = this.quebraBiscoito.bind(this)
-    this.frases=[
-      'frase 1', 'frase 2', 'frase 3', 'frase 4', 'frase 5', 
-      'frase 6', 'frase 7', 'frase 8', 'frase 9', 'frase 10', 
-    ]
-  }
+      numero: 0,
+      botao: 'Vai'
+    }
+    this.timer = null
+    this.vai = this.vai.bind(this)
+    this.limpar = this.limpar.bind(this)
+    }
 
-  quebraBiscoito(){
+  vai(){
     let state = this.state;
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
-    state.textoFrase = '"'+ this.frases[numeroAleatorio] + '"'
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null
+      state.botao = "Vai"
+    } else{
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero+= 0.1
+        this.setState(state)
+      }, 100);
+      state.botao = "Pausar"
+    }
+
     this.setState(state)
+
     
   }
+  
+  limpar(){
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null
+    }
 
+    let state = this.state
+    state.numero = 0
+    state.botao = "Vai"
+    this.setState(state)
+  }
+  
   render(){
     return(
       <div className='container'>
-        <img src={require('./assets/biscoito.png')} className='img'/>
-        <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
-        <h3 className='textoFrase'>{this.state.textoFrase}</h3>
-      </div>
-    )
-  }
-}
-
-class Botao extends Component{
-  render(){
-    return(
-      <div>
-        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+        <img src={require('./assets/cronometro.png')} className='img'/>
+        <a className='timer'>{this.state.numero.toFixed(1)}</a>
+        <div className="areaBtn">
+        <a className='botao' onClick={this.vai}>{this.state.botao}</a>
+        <a className='botao' onClick={this.limpar}>Limpar</a>
+        </div>
       </div>
     )
   }
