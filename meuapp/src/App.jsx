3d072fa193aee48b7cd364843251a4a7aabe76a1
Fernando <kinggprobr@gmail.com>
2024-03-01 16:31:24 -0300

    aula 53

--- meuapp/src/App.jsx
+++ meuapp/src/App.jsx
@@ -1,43 +1,44 @@
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
+import './estilo.css'
+
+// sujeitoprogramador.com/rn-api/?api=posts
 
 function App(){
 
-  const [tarefas, setTarefas] = useState([])
-  const [input, setInput] = useState('')
-
+  const [nutri, setNutri] = useState([])
 
   useEffect(()=>{
-    const tarefasStorage = localStorage.getItem('tarefas')
-    console.log(tarefasStorage)
-    setTarefas(JSON.parse(tarefasStorage))
+    let url = "https://sujeitoprogramador.com/rn-api/?api=posts"
+
+    function loadApi(){
+      fetch(url)
+      .then((r)=> r.json())
+      .then((json) =>{
+        setNutri(json)
+      })
+    }
+
+    loadApi();
   },[])
 
-  useEffect(()=>{
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-  },[tarefas])
-
-
-  const addTarefa = useCallback(() => {
-      setTarefas([...tarefas, input])
-      setInput('')
-  },[tarefas, input])
-
-  const totalTarefas = useMemo(()=> tarefas.length, [tarefas])
-
-
-
-
   return(
-    <div>
-      <ul>
-        {tarefas.map(tarefa =>(
-          <li key={tarefa}>{tarefa}</li>
-          ))}
-      </ul>
-      <br />
-      <strong>Você tem {totalTarefas} tarefas</strong><br />
-      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
-      <button onClick={addTarefa}>Adicionar Tarefa</button>
+    <div className='container'>
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map((item)=>{
+        return(
+          <article key={item.id} className='post'>
+            <strong className='titulo'>{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className='capa'/>
+            <p className='subtitulo'>
+              {item.subtitulo}
+            </p>
+            <a className='botao'>Acessar</a>
+          </article>
+        )
+      })}
     </div>
   )
 }
