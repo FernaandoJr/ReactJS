c58b0ab5e9e973f301e553065f8bfd62459080c5
Fernando <kinggprobr@gmail.com>
2024-04-18 15:12:20 -0300

    aula 63

--- primeflix/src/routes.js
+++ primeflix/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 import Header from './components/Header'
+import Error from './pages/Error'
 
 function RoutesApp(){
     return(
@@ -12,6 +13,7 @@ function RoutesApp(){
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/filme/:id' element={<Filme/>}/>
+                <Route path='*' element={<Error/>}/>
             </Routes>
         </BrowserRouter>
     )
